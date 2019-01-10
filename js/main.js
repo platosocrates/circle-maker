4fb2ed8d4da1b2fcc69d1256bb74d8657b644f04
Lee <liam.eakins@outlook.com>
2019-01-10 04:21:14 -0600

    Generate random color
    Also changed left and top coordinates to  percentages rather than pixels

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Function generates random number and multiplies it by 1520
-function generateRandom() {
-    return (Math.random() * 1520);
+function getRandomPercent() {
+    return (Math.random() * 100) + "%";
 }
 
 // Function generates a random hexadecimal number
 function getRandomColor() {
     let letters = "0123456789ABCDEF";
     let color = "#";
-    for(let i = 0, i < 6, i++) {
+    for(let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
@@ -15,7 +15,7 @@ function getRandomColor() {
 // On click creates new circle with random coordinates
 $(".btn").on("click", () => {
     var $ball = $("<div></div>").addClass("ball");
-    $ball.css({"left": generateRandom(), "top": generateRandom()});
+    $ball.css({"background-color": getRandomColor(), "left": getRandomPercent(), "top": getRandomPercent()});
     $("body").append($ball);
 });
 
